083ba2b998bd1d3fb93c8806bbd7e021b59d391b
etakoua <etakoua@visionca.com>
2026-02-25 20:04:55 +0100

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./pages/landing/landing.component').then((m) => m.LandingComponent),
+  },
+  {
+    path: 'create',
+    loadComponent: () => import('./pages/create-card/create-card.component').then((m) => m.CreateCardComponent),
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./pages/dashboard/dashboard.component').then((m) => m.DashboardComponent),
+  },
+  {
+    path: 'card/:id',
+    loadComponent: () => import('./pages/view-card/view-card.component').then((m) => m.ViewCardComponent),
+  },
+  {
+    path: 'contacts',
+    loadComponent: () => import('./pages/saved-contacts/saved-contacts.component').then((m) => m.SavedContactsComponent),
+  },
+  {
+    path: '**',
+    loadComponent: () => import('./pages/not-found/not-found.component').then((m) => m.NotFoundComponent),
+  },
+];
